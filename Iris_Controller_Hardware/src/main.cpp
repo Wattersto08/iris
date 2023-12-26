e79f5bbd1292493bfbc0aef7e580f3dea0f1ebe7
Tom <tom.j.watters@gmail.com>
2023-12-26 12:04:15 +0000

    added a new dir for proven working tests "iris_uros_tests" for reference - current working test is to configure multiple pub/subs -> desired target and actual positions for XY axis

--- Iris_Controller_Hardware/src/main.cpp
+++ Iris_Controller_Hardware/src/main.cpp
@@ -4,13 +4,27 @@
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
+#include <rcl/error_handling.h>
 
 #include <std_msgs/msg/int32.h>
+
+#include <stdio.h>
+
+#include <std_msgs/msg/int32.h>
+
+
 
 #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
 #error This example is only avaliable for Arduino framework with serial transport.
 #endif
 
+//publisher setup 
+rcl_subscription_t subscriber;
+//rcl_subscription_t subscriber;
+//std_msgs__msg__Int32 msg;
+
+
+// publisher setup 
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 msg;
 
@@ -20,14 +34,32 @@ rcl_allocator_t allocator;
 rcl_node_t node;
 rcl_timer_t timer;
 
+#define LED1 = 23;
+#define LED2 = 22;
+
+
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
 
 // Error handle loop
 void error_loop() {
   while(1) {
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
     delay(100);
   }
+}
+
+void subscription_callback(const void * msgin)
+  RCLC_UNUSED(last_call_time);
+{  
+  if (timer != NULL) {
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+  digitalWrite(LED2, (msg->data == 0) ? LOW : HIGH);  
+    msg.data++;
+  }
+}
 }
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
@@ -44,6 +76,13 @@ void setup() {
   set_microros_serial_transports(Serial);
   delay(2000);
 
+  // error trap led
+  pinMoce(LED1,OUTPUT);
+
+  // subscriber led 
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
   allocator = rcl_get_default_allocator();
 
   //create init_options
@@ -51,6 +90,14 @@ void setup() {
 
   // create node
   RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+
+    // create subscriber
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_arduino_subscriber"));
+
 
   // create publisher
   RCCHECK(rclc_publisher_init_default(
@@ -60,7 +107,7 @@ void setup() {
     "micro_ros_platformio_node_publisher"));
 
   // create timer,
-  const unsigned int timer_timeout = 1000;
+  const unsigned int timer_timeout = 100;
   RCCHECK(rclc_timer_init_default(
     &timer,
     &support,
@@ -70,6 +117,7 @@ void setup() {
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
 
   msg.data = 0;
 }
